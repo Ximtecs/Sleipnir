2c5dc2b86df70439a94f0c94f7683fbc67d01a4c
ximtecs <michael.haahr101293@gmail.com>
2024-03-10 07:46:25 +0100

    [DEV] changed task.execute to task.update

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -7,6 +7,6 @@ void MainProgram::run() {
     // Placeholder functionality for demonstration
     // This function would be where the main program's logic is implemented.
     Task task;
-    task.execute();
+    task.update();
 
 }
