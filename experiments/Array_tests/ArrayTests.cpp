65db55182dd73c39bd2ab5f9e0dabf2dd1a66942
ximtecs <michael.haahr101293@gmail.com>
2024-03-11 07:09:50 +0100

    [DEV] added GPU alloc to test. Does not work yet

--- experiments/Array_tests/ArrayTests.cpp
+++ experiments/Array_tests/ArrayTests.cpp
@@ -1,21 +1,69 @@
 #include "ArrayCPU.hpp"
+#include "ArrayGPU.hpp"
 #include <iostream>
+#include <omp.h>
+#include <chrono>
+#include <thread>
 
 int main() {
-    // Create a StaticArrayCPU of size 10
-    ArrayCPU<int> array(10);
 
+    // Get the number of OpenMP devices
+    int numDevices = omp_get_num_devices();
+    std::cout << "Number of OpenMP devices: " << numDevices << std::endl;
+
+    // Get the default OpenMP device
+    int defaultDevice = omp_get_default_device();
+    std::cout << "Default OpenMP device: " << defaultDevice << std::endl;
+
+
+    int size = 1000000; 
+
+    // Create a StaticArrayCPU of size 
+    ArrayCPU<int> array(size);
     std::cout << "array.size() = " << array.getSize() << std::endl;
 
-    // Fill the array with some values
-//    for (std::size_t i = 0; i < array.size(); ++i) {
-//        array[i] = i * 2;
-//    }
-//
-//    // Print the contents of the array
-//    for (std::size_t i = 0; i < array.size(); ++i) {
-//        std::cout << "array[" << i << "] = " << array[i] << std::endl;
-//    }
+    // Start the timer
+    auto startCPU = std::chrono::high_resolution_clock::now();
+
+    // Initialize the array with a value
+    array.initValue(5);
+
+    // Stop the timer and calculate the elapsed time
+    auto stopCPU = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsedCPU = stopCPU - startCPU;
+    std::cout << "Time to update CPU array: " << elapsedCPU.count() << " seconds\n";
+
+
+    // Create a StaticArrayGPU of size 
+    ArrayGPU<int> arrayGPU(size);
+    std::cout << "arrayGPU.size() = " << arrayGPU.getSize() << std::endl;
+
+
+
+    int* data = static_cast<int*>(omp_target_alloc(size * sizeof(int), omp_get_default_device()));
+
+
+    // Start the timer
+    auto startGPU = std::chrono::high_resolution_clock::now();
+
+    // Initialize the array with a value
+    //arrayGPU.initValue(5);
+
+    #pragma omp target teams distribute parallel for is_device_ptr(data)
+    for (std::size_t i = 0; i < size; ++i) {
+        data[i] = 5; // Use the passed value for initialization
+    }
+
+    
+
+    // Stop the timer and calculate the elapsed time
+    auto stopGPU = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsedGPU = stopGPU - startGPU;
+    std::cout << "Time to update GPU array: " << elapsedGPU.count() << " seconds\n";
+
+
+
+    std::this_thread::sleep_for(std::chrono::seconds(10));
 
     return 0;
 }
