92e4934499c775c3f55996b4835976a537ca9959
ximtecs <michael.haahr101293@gmail.com>
2024-03-12 19:54:29 +0100

    [DEV] simpleRun now creates Patch types. Tests polymorphism in the MainProgram

--- experiments/SimpleRun/simpleRun.cpp
+++ experiments/SimpleRun/simpleRun.cpp
@@ -1,13 +1,22 @@
 // test1.cpp - Test for the DISPATCH_CPP framework
 #include <iostream>
+#include "patch.hpp"
 #include "main.hpp"
 
 int main() {
     std::cout << "Running simpleRun for SLEIPNIR framework." << std::endl;
 
     MainProgram program;
-    program.initialize(10); // Initialize with 10 tasks
+    //program.initialize(10); // Initialize with 10 tasks
 
+    for (int i = 0; i < 10; i++) {
+        Patch* task = new Patch();
+        task->SetPatchSize(10000000);
+        task->AllocateMemory();
+        program.pushTask(task);
+    }
+
+    
     program.run(); // This will now also print the IDs of the initialized tasks
 
 
